8ba7cefc732bab1b7a5663d771610d7d52b572ff
sanurafilter <105334889+sanurafilter@users.noreply.github.com>
2022-05-11 09:55:27 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -8,7 +8,7 @@ from utils import get_size, temp, get_settings
 from Script import script
 from pyrogram.errors import ChatAdminRequired
 
-"""-----------------------------------------https://t.me/filmexcloudupdates --------------------------------------"""
+"""-----------------------------------------https://t.me/TWN_BOTS --------------------------------------"""
 
 @Client.on_message(filters.new_chat_members & filters.group)
 async def save_group(bot, message):
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/filmexcloudupdates')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/TWN_BOTS')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
